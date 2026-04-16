b67ab69a5c7844c45f4651dc5e9b9d1448ff1473
acedenosachez <acedenosanchez@uade.edu.ar>
2026-04-15 22:00:44 -0300

    Remove x objeto , clear , contains

--- src/listModule/SimpleArrayList.java
+++ src/listModule/SimpleArrayList.java
@@ -18,11 +18,9 @@ public class SimpleArrayList<E> implements SimpleList<E> {
         elements = (E[]) new Object[capacity];
     }
 
-
-
     private void validateIndex(int index) {
         if (index < 0 || index >= size)
-            throw new IndexOutOfBoundsException("Index was less than 0, or greater than size" );
+            throw new IndexOutOfBoundsException("Index was less than 0, or greater than size");
     }
 
     @Override
@@ -32,7 +30,15 @@ public class SimpleArrayList<E> implements SimpleList<E> {
 
     @Override
     public void add(int index, E element) {
-
+        //Verificamos que el indice sea correcto
+        if (index < 0 || index > elements.length) {
+            throw new IndexOutOfBoundsException("Index was less than 0, or greater than size");
+        } else if (size + 1 > elements.length) {
+            throw new IndexOutOfBoundsException("Too many elements");
+        }
+        elements[index] = element;
+        size++;
+        System.out.println("The element " + element.toString() + " has been added");
     }
 
     @Override
@@ -42,16 +48,42 @@ public class SimpleArrayList<E> implements SimpleList<E> {
 
     @Override
     public boolean remove(Object object) {
+        //podriamos ahorrarnos este if si contains devolviera el indice
+        if (contains(object)) {
+            int index = -1;
+            for (int i = 0; i < size; i++) {
+                if (elements[i].equals(object)) {
+                    index = i;
+                }
+            }
+            if (index >= 0) {
+                for (int i = index; i < size-1; i++) {
+                    elements[i] = elements[i + 1];
+                }
+                elements[size-1] = null;
+                size--;
+            }
+            return true;
+        }
         return false;
     }
 
     @Override
     public void clear() {
-
+        E[] elements = null;
+        size = 0;
     }
 
     @Override
+
+    //Comentarios para el profe: Mayor eficiencia si devolvemos directamente el indice donde se encuentra el valor y en caso de no encontrarse devolver -1 , así no se tiene que iterar nuevamente
     public boolean contains(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == object) {
+                return true;
+            }
+
+        }
         return false;
     }
 
@@ -75,6 +107,4 @@ public class SimpleArrayList<E> implements SimpleList<E> {
         return false;
     }
 }
-
-
 
