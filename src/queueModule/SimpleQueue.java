2d248299bb84c655ccc11f683f74332eebcc870b
Martino <mcirielli@uade.edu.ar>
2026-04-20 18:41:05 -0300

    StackExercise resolución.

--- src/queueModule/SimpleQueue.java
+++ src/queueModule/SimpleQueue.java
@@ -2,7 +2,7 @@ package queueModule;
 
 public interface SimpleQueue<E>{
 
-    public void unqueue();
+    public void enqueue(E element);
     public E dequeue();
     public boolean isEmpty();
     public int size();
