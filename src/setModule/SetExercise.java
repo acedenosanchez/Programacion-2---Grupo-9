4d43665e5c057019370bfca80d75f474e46a11ce
Martino <mcirielli@uade.edu.ar>
2026-05-22 14:47:53 -0300

    Creación de clases y interfaz de TP Sets con sus bases.

---
+++ src/setModule/SetExercise.java
@@ -0,0 +1,17 @@
+package setModule;
+
+import aplication.Exercise;
+
+import java.util.Scanner;
+
+public class SetExercise extends Exercise {
+
+    public SetExercise(Scanner scnr) {
+        super(scnr);
+    }
+
+    @Override
+    protected void exerciseLogic() {
+
+    }
+}
